507d93a81e54cc75ab23eeeaef8b55e5a6c0ea51
Mairead Meagher <mmeagher@wit.ie>
2025-10-07 21:37:40 +0100

    bits

---
+++ topic05-loops/unit-2 - labs - tutorials/book-1/archives/Loops/ForLoop.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class ForLoop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ForLoop
+  {
+    public static void main(String[] args){
+        
+       for (int i = 1; i<=4; i++) {
+           
+          System.out.println("Hello World");
+        }
+}
+  }
---
+++ topic05-loops/unit-2 - labs - tutorials/book-1/archives/Loops/WhileLoop.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class ForLoop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WhileLoop
+{
+    public static void main(String[] args){
+    int i = 1;
+    while (i <= 4)
+    {
+    System.out.println("Hello World");
+    i++;
+    }
+}
+}
