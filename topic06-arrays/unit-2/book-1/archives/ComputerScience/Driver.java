856b40bb3206c3b4392db8db95f04d5c66f429b7
Mairead Meagher <mmeagher@wit.ie>
2025-10-17 22:51:44 +0100

    update icon for course

---
+++ topic06-arrays/unit-2/book-1/archives/ComputerScience/Driver.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Driver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Driver
+{
+    Person[] students;
+    public static void main(String[] args) {
+        new Driver();
+    }
+        
+    Driver(){ 
+        
+        students = new Person[20];
+        
+        // Person me = new Person("Joe", "Bloggs", 31);
+        
+        // me.printFullName();
+        // me.printAge();
+        
+        // System.out.println("Happy Birthday! You were " + me.happyBirthday() + 
+        // " years old. Now you are " +  me.getAge());
+    
+      
+    }
+}
