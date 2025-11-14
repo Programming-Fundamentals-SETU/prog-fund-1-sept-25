c25d1d2bc85acf7145a648848cf9f95aea3fb9e8
siobhan roche <siobhanpowerroche@gmail.com>
2025-11-14 16:50:45 +0000

    updated book week 9- typos2

--- topic09-menus-and-persistence/unit-2/book/archives/LoopExercises/Driver.java
+++ topic09-menus-and-persistence/unit-2/book/archives/LoopExercises/Driver.java
@@ -10,7 +10,7 @@ public class Driver
     }
     
     public Driver(){
-        System.out.println(loops.simpleWhile());
+        runMenu();
     }
     
     public void createNames(){
@@ -28,4 +28,40 @@ public class Driver
           
         }
     }
+    
+    private int mainMenu(){
+        System.out.print("""
+                Menu
+               ---------
+                  1) Simple Loop
+                  2) Create the Array
+                  3) Print the Names
+                  0) Exit
+               ==>> """);
+        int option = input.nextInt();
+        return option;
+    }
+
+    private void runMenu(){
+        int option = mainMenu();
+        while (option != 0){
+            switch (option){
+                case 1 -> System.out.println(loops.simpleWhile());
+                case 2 -> createNames();
+                case 3 -> System.out.println(loops.listNames());
+            
+                default -> System.out.println("Invalid option entered: " + option);
+            }
+            //pause the program so that the user can read what we just printed to the terminal window
+            System.out.println("\nPress enter key to continue...");
+            input.nextLine();
+            input.nextLine(); //second read is required - bug in Scanner class;
+            // display the main menu again
+            option = mainMenu();
+        }
+        //the user chose option 0, so exit the program
+        System.out.println("Exiting...bye");
+        System.exit(0);
+    }
+    
 }
